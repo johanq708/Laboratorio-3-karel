f74a86a33bf38feab427f21af71154312fa8c4b6
johit <johit@DESKTOP-HQPQDH2>
2017-03-14 18:31:51 -0500

    pizza

--- src/pizzabase/Pizza.java
+++ src/pizzabase/Pizza.java
@@ -6,8 +6,9 @@
 package pizzabase;
 import java.util.Scanner;
 /**
- *
+ * @since 12 de marzo de 2017
  * @author Johan Quiñonez
+ * @version 1.0
  */
 public class Pizza {
     Scanner teclado= new Scanner(System.in);
@@ -15,6 +16,13 @@ public class Pizza {
     private Tamaño tamaño;
     private int vpizza;
     private Predeterminada[] pre;
+    /**
+    * constructor 
+    * @param tamaño es el tamaño de la pizza
+    * @param ing son los ingredientes de la pizza
+    * @param pre son las pizzas ya predeterminadas que se pueden escoger
+    * @param vpizza es el valor de la pizza
+    */
 
     public Pizza() {
         this.tamaño = tamaño;
@@ -30,49 +38,55 @@ public class Pizza {
         this.pre[2]= new Predeterminada(3000,"mexicana");
         this.vpizza=vpizza;
     }
-
+    /**
+    *@return el tamaño 
+    */
     public Tamaño getTamaño() {
         return tamaño;
     }
-
+    /**
+    *@return el valor de la pizza 
+    */
     public int getVpizza() {
         return vpizza;
     }
-    
+    /**
+    *@param t   
+    */
     public void dartamaño(){
-        System.out.println("Que tamaño de pizza desea, pequeño=1, mediana=2 o grande=3");
+        System.out.println("Que tamaño de pizza desea, pequeño=3, mediana=6 o grande=9");
         int t=teclado.nextInt();
-        if(t==1){
-            tamaño.setTam(3);
-        }else
-        if(t==2){
-            tamaño.setTam(6);
-        }else
-        if(t==3){
-            tamaño.setTam(9);
-        }else{
-            System.out.println("tamaño invalido");
+        tamaño.setTam(t);
         }
     }
-    
+    /**
+    *@return el valor del ingrediente escogido
+    */
     public int valoringrediente(){
         System.out.println("¿Que ingrediente desea?: 1=queso,2=jamon,3=carne,4=pollo,5=salami,6=piña,7=maiz");
         int v=teclado.nextInt();
         return vpizza=ing[v-1].getPrecio();
     }
-    
+    /**
+    *@return la suma del valor de los ingredientes escogidos
+    */
     public int masingrediente(){
         System.out.println("¿Que otro ingrediente desea?: 1=queso,2=jamon,3=carne,4=pollo,5=salami,6=piña,7=maiz");
         System.out.println("no repita el ingrediente que escogió anteriormente");
         int v=teclado.nextInt();
         return vpizza+=ing[v-1].getPrecio();
     }
-    
+    /**
+    *@return el valor de la pizza predeterminada 
+    */
     public int pizzap(){
         System.out.println("¿Que pizza desea?: 1=carnes, 2=hawaiyana, 3=mexicana");
         int v=teclado.nextInt();
         return vpizza=pre[v-1].getPrecio();
     }
+/**
+    *@return el valor de la pizza escogida o la suma de los ingredientes elegidos multiplicado por el tamaño 
+    */
     public int pizzapt(){
         return vpizza=vpizza*tamaño.getTam();
     }
