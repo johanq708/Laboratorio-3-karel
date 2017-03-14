538723b5d7777b5c09950a8148cfe802126c6b49
johit <johit@DESKTOP-HQPQDH2>
2017-03-14 18:01:17 -0500

    pizza

---
+++ src/pizzabase/Tamaño.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pizzabase;
+
+/**
+ *
+ * @author johit
+ */
+public class Tamaño {
+    private int tam;
+
+    public Tamaño() {
+        this.tam = tam;
+    }
+
+    public int getTam() {
+        return tam;
+    }
+
+    public void setTam(int tam) {
+        this.tam = tam;
+    }
+    
+}
