b5413e428355f33c259ec0f1c5b72176ae42858e
johanq708 <jdquinonezr@unal.edu.co>
2017-03-14 22:31:53 -0500

    Update and rename Tamaño.java to NewClass.java

---
+++ src/pizzabase/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Laberinto;
+import becker.robots.*;
+/**
+ * @since 12 de marzo de 2017
+ * @author Johan Daniel Quiñonez Ruiz
+ * @version 1.0
+ */
+public class NewClass {
+    public static void main(String[]args){
+    Resolver resolver=new Resolver();
+    resolver.resuelve();
+    }
+}
